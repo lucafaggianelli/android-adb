3ab59de40f7e6bd1c2b17b2c7d170d3574a154a0
Luca Faggianelli <luca.faggianelli@datalogic.com>
2016-05-18 09:56:23 +0200

    generated from atom

---
+++ spec/android-adb-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AndroidAdbView from '../lib/android-adb-view';
+
+describe('AndroidAdbView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
